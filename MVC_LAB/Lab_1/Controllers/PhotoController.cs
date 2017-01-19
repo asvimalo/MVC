ba2b6ed2c3c7a0e33cf08af4d3c2e5c157d7e18d
asvimalo <asvimalo@gmail.com>
2017-01-19 19:30:50 +0100

    HomeController Done

---
+++ MVC_LAB/Lab_1/Controllers/PhotoController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LAB_DAL.Repo;
+
+namespace Lab_1.Controllers
+{
+    public class PhotoController : Controller
+    {
+        PhotoRepository repo = new PhotoRepository();
+        // GET: Photo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
